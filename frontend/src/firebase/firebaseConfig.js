7a0798496d6554bd17c52bc3dba1db7c247aa296
ALLURKARSUSHANTH <sushanth9524@gmail.com>
2025-06-26 14:38:45 +0530

    update for firebase file

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_auth_Domain,
-  projectId: import.meta.env.VITE_project_Id ,
-  storageBucket: import.meta.env.VITE_storage_Bucket,
-  messagingSenderId: import.meta.env.VITE_messaging_SenderId ,
-  appId: import.meta.env.VITE_app_Id,
-  measurementId:import.meta.env.VITE_measurement_Id
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
 export { app, auth };
